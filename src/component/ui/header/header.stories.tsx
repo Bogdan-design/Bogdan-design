4ed86ccfc97b08987c338d646eb2e6c6f037baff
Bogdan-design <bogdanbw@gmail.com>
2023-08-15 00:25:15 +0200

    refactor Header component

--- src/component/ui/header/header.stories.tsx
+++ src/component/ui/header/header.stories.tsx
@@ -2,25 +2,13 @@ import { Meta, StoryObj } from '@storybook/react'
 
 import { Header } from './'
 
-export default {}
 const meta = {
   title: 'Components/Header',
   component: Header,
   tags: ['autodocs'],
 } satisfies Meta<typeof Header>
 
+export default meta
 type Story = StoryObj<typeof meta>
 
-export const IsLogin: Story = {
-  args: {
-    label: 'Click here',
-    disabled: false,
-  },
-}
-
-export const IsOut: Story = {
-  args: {
-    label: 'Click here',
-    disabled: false,
-  },
-}
+export const IsLogin: Story = {}
--- src/component/ui/header/header.tsx
+++ src/component/ui/header/header.tsx
@@ -1,10 +1,29 @@
+import { useState } from 'react'
+
+import logo from '../../../assets/icon/Logo.png'
+import { Button } from '../../ui'
+
 import s from './header.module.scss'
 
 export const Header = () => {
+  const [isLogged, setIsLoget] = useState(false)
+  const DropDawnMenu = () => {
+    return <>Hello</>
+  }
+
+  const signInHandler = () => {
+    setIsLoget(true)
+  }
+
   return (
-    <div className={s.header}>
-      <div></div>
-      <div></div>
-    </div>
+    <header className={s.header}>
+      <a href={logo}></a>
+      {isLogged && <DropDawnMenu />}
+      {!isLogged && (
+        <Button variant={'primary'} onClick={signInHandler}>
+          Sign In
+        </Button>
+      )}
+    </header>
   )
 }
