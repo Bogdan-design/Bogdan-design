af2548dd38101c1fd55433d5f92768895fea168b
Bogdan-design <bogdanbw@gmail.com>
2023-08-15 12:40:11 +0200

    refactor SignUp component

--- src/component/layout/index.tsx
+++ src/component/layout/index.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom'
 
 import { Header } from './header'
+import s from './layout.module.scss'
 
 export const Layout = () => {
   return (
-    <div>
+    <div className={s.layout}>
       <Header />
       {/*<div aria-hidden />*/}
       <Outlet />
