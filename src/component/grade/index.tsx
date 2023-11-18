cb07f07c8ba8a171196aaa010ec2ca26cc06a0a0
Bogdan-design <bogdanbw@gmail.com>
2023-11-18 22:36:29 +0100

    add logic for rating

--- src/component/grade/index.tsx
+++ src/component/grade/index.tsx
@@ -2,7 +2,6 @@ import EmptyStar from '../../assets/icon/emptyStar'
 import Star from '../../assets/icon/star'
 
 export const Grade = ({ rating }: { rating: number }) => {
-  console.log(rating)
   const rateComponent = () => {
     return Array.from({ length: 5 }, (_, i) => <EmptyStar key={i} />).fill(<Star />, 0, rating)
   }
